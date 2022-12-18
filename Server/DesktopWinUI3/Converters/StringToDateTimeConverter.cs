8a0137fcd7f273ae26023a74b73fadda06b4a5a3
Igor Motyka <igor_motyka@mail.com>
2022-12-18 19:28:44 +0100

    Added converter between string and datetime

---
+++ Server/DesktopWinUI3/Converters/StringToDateTimeConverter.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WinRT;
+
+namespace DesktopWinUI3.Converters
+{
+    public sealed class StringToDateTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if(value is null) return null;
+
+            var x = value.As<DateTime>().ToString("dd/MM/yyyy HH:mm");
+            return x;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            if (value is null) return null;
+
+            var str = value.As<string>();
+
+            if(string.IsNullOrEmpty(str)) return null;
+
+            return DateTime.Parse(str);
+        }
+    }
+}
